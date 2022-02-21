50b5fa47ad1d1e57e84b2e520db8b1374fde83f4
jzx <945627077@qq.com>
2022-02-21 15:42:34 +0800

    '增加cpp访问hostdom测试'

--- WhIde.cpp
+++ WhIde.cpp
@@ -4,25 +4,95 @@
 #include "stdafx.h"
 #include "sciter-x.h"
 #include "sciter-x-window.hpp"
+#include "sciter-x-behavior.h"
+
+using namespace sciter;
 
 class frame : public sciter::window {
 public:
 	frame() : window(SW_TITLEBAR| SW_RESIZEABLE | SW_CONTROLS | SW_MAIN | SW_ENABLE_DEBUG) {}
+	
+	SOM_PASSPORT_BEGIN(frame)
+		SOM_FUNCS(
+			SOM_FUNC(nativeMessage)
+		)
+	SOM_PASSPORT_END
+
+		// function expsed to script:
+	sciter::string  nativeMessage() { 
+		dom::element root = dom::element::root_element(get_hwnd());
+		dom::element s = root.find_first(".testaccess");
+		s.set_text(L"Hello world");
+		return WSTR("Hello C++ World");
+	}
 };
 
 #include "resources.cpp"
 
+namespace sciter {
+
+
+class editor1 : public sciter::event_handler {
+public:
+	HWND     hwnd;
+
+	HELEMENT self; // note: weak ref (not addrefed)
+	
+
+	virtual void attached(HELEMENT he) {
+		self = he;
+		dom::element el = he;
+	}
+	virtual void detached(HELEMENT he) {
+		
+	}
+
+	bool select(sciter::value   indexOrName) {
+		return true;
+	}
+	sciter::string get_current() {
+		return sciter::string();
+	}
+
+	SOM_PASSPORT_BEGIN(editor1)
+		SOM_FUNCS(
+			SOM_FUNC(select),
+		)
+		SOM_PROPS(
+			SOM_RO_VIRTUAL_PROP(current, get_current),
+			)
+	SOM_PASSPORT_END
+
+	sciter::value get_text() {
+		std::wstring text;
+		// GetWindowText(hwnd) & friends here
+		return sciter::value(text);
+	}
+
+	sciter::value set_text(const sciter::value& text_val) {
+		std::wstring text = text_val.get(L"11111111111111111111");
+		// SetWindowText(hwnd) & friends here
+		return sciter::value(); // returns undefined value, a.k.a. void
+	}
+};
+
+struct editor_factory : public sciter::behavior_factory {
+	editor_factory() : behavior_factory("editor1") {}
+	virtual sciter::event_handler* create(HELEMENT he) { 
+		return new editor1(); }
+};
+
+editor_factory editor_factory_instance;
+
+}
+
 int uimain(std::function<int()> run) {
-	SciterSetOption(NULL, SCITER_SET_UX_THEMING, TRUE);
-	SciterSetOption(NULL, SCITER_SET_DEBUG_MODE, TRUE);
-	SciterSetOption(NULL, SCITER_SET_SCRIPT_RUNTIME_FEATURES,
-		ALLOW_FILE_IO |
-		ALLOW_SOCKET_IO |
-		ALLOW_EVAL |
-		ALLOW_SYSINFO);
 	sciter::archive::instance().open(aux::elements_of(resources));
 	frame* pwin = new frame();
-	pwin->load_file(WSTR("./res/src/index.html"));
+	pwin->load(WSTR("file://E:\\WhIde\\res\\test\\index.html"));
+
+
+	//pwin->load(WSTR("this://app/index.html"));
 	pwin->expand();
 
 	return run();
--- res/src/views/Term.js
+++ res/src/views/Term.js
@@ -2,7 +2,8 @@
 export class Term extends Element  {
     render (){
         return <div>
-            控制台
+            控制台11
+            <editor style="behavior: editor; display: block;width: *;  height: *;"/>
         </div>
     }
 }
